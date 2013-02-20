78155d24555a4f7accf4bc12674dead22fcb3f2b
Cristian Sebastian Rocha <crocha@kekkaishi.jjdiazarana.net>
2013-02-20 12:57:44 -0300

    [FIX] "from osv import" must be replaced by "from openerp.osv import"

--- invoice.py
+++ invoice.py
@@ -18,7 +18,7 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from osv import fields, osv
+from openerp.osv import fields, osv
 from cache_bind import get_bind
 from stub.Service_client import *
 from stub.Service_types import *
--- journal.py
+++ journal.py
@@ -18,7 +18,7 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from osv import fields, osv
+from openerp.osv import fields, osv
 from stub.Service_client import *
 from stub.Service_types import *
 from tools.translate import _
